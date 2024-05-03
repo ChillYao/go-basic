337af9885f3e954ee67746a4a88bed94030ff7b2
tong.yao <tong.yao@check24.de>
2024-05-03 08:18:36 +0200

    the first go program

---
+++ variables/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var myName string = "YAO"
+	myName := "YAO"
+	myInt := 10
+	myFloat := 3.14
+	fmt.Printf("Hello, my name is %s\n my int is %d\n my float is %f", myName, myInt, myFloat)
+}
