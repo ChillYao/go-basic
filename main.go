b23428e1b11c90e21066ba730281d683ba75a218
tong.yao <tong.yao@check24.de>
2024-05-08 11:12:14 +0200

    practice for module imports

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"goBasic/imports"
 )
 
 func main() {
-	fmt.Println("h w")
+	newTicket := imports.Ticket{
+		ID:    123,
+		Event: "FEM course",
+	}
+	newTicket.PrintEvent()
+	fmt.Println(newTicket)
 }
